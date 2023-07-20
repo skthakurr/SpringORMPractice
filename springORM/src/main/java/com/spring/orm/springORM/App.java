65ab33bfdf2025aa4d7cfeaa366860f0f577fcbc
skthakurr <sanojbharti38@gmail.com>
2023-07-21 01:48:17 +0530

    first commit

---
+++ springORM/src/main/java/com/spring/orm/springORM/App.java
@@ -0,0 +1,13 @@
+package com.spring.orm.springORM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
